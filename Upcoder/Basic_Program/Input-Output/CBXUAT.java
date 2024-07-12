11f68bcd99c52dd305447285f8190f01a5ce4855
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-12 15:03:24 +0700

    Create CBXUAT.java

---
+++ Upcoder/Basic_Program/Input-Output/CBXUAT.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+public class Main {
+    public static void main() {
+        System.out.println("EM XIN HUA: KHONG BAO GIO");
+        System.out.println("COPY BAI (hoac chia se bai)");
+        System.out.println("NEU VI PHAM SE BI 0 DIEM TOAN BAI THI");
+        
+    }
+}
+
