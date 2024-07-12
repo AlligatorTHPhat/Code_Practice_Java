168fc9baafeb33f7eff669a5b505e5875123a359
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-12 19:02:45 +0700

    Update INAN.java

--- Upcoder/Basic_Program/If-Else/INAN.java
+++ Upcoder/Basic_Program/If-Else/INAN.java
@@ -1,1 +1,28 @@
 
+import java.util.Scanner;
+
+public class Main {
+    public static boolean check(int T, long P, long C) {
+        return (1 <= T && T <= 2 && 1 <= P && P <= Math.pow(10,9) && 1 <= C && C <= Math.pow(10,9)); 
+    }
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+       
+       int T = scanner.nextInt();
+       long P = scanner.nextLong();
+       long C = scanner.nextLong();
+       
+       long result;
+        
+        if(check(T,P,C)) {
+            if(T == 1) {
+                System.out.print(P * C);
+            } else if(T == 2) {
+                System.out.print(((P / 2) + (P % 2)) * C);
+            }
+        }  else System.out.print("Error");
+        
+        scanner.close();
+    }
+}
