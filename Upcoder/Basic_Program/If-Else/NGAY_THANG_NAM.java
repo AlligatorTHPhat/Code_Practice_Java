e3302956a49dbdea451b6fa1448bcbc649d39c76
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-12 21:33:57 +0700

    Create NGAY_THANG_NAM.java

---
+++ Upcoder/Basic_Program/If-Else/NGAY_THANG_NAM.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean check_leap_year(int year) {
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int day = scanner.nextInt();
+        int month = scanner.nextInt();
+        int year = scanner.nextInt();
+        
+        if (year < 1900 || month < 1 || month > 12) {
+            System.out.print("NO");
+            return;
+        } 
+        
+        int max_day;
+        switch(month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                max_day = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                max_day = 30;
+            case 2:
+                max_day = check_leap_year(year) ? 29 : 28;
+                break;
+            default:
+                max_day = 0;
+        }
+        
+        if (day >= 1 && day <= max_day) {
+            System.out.print("YES"); return;
+        } else {
+            System.out.print("NO"); return;
+        }
+    }
+}
