99898a50190defe6bae2aa148231813de0118084
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-12 15:51:04 +0700

    Create NHAPXUAT7.java

---
+++ Upcoder/Basic_Program/Input-Output/NHAPXUAT7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        float n = scanner.nextFloat();
+        
+        System.out.printf("%.3f",n);
+        
+    }
+}
